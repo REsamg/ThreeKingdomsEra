4d87c75c1bd8e4f6d3c2ca9c447d6a6654ccb104
RE didi <1030759339@qq.com>
2020-08-27 15:49:49 +0800

    GUI

--- src/main/java/com/re_she/mod/client/gui/GuiContainerDemo.java
+++ src/main/java/com/re_she/mod/client/gui/GuiContainerDemo.java
@@ -1,9 +1,11 @@
 package com.re_she.mod.client.gui;
 
+import com.re_she.mod.Fmltutor;
 import com.re_she.mod.inventory.ContainerDemo;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.util.ResourceLocation;
 
 @SideOnly(Side.CLIENT)
 public class GuiContainerDemo extends GuiContainer
@@ -16,13 +18,20 @@ public class GuiContainerDemo extends GuiContainer
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
     {
-        // TODO
+
+        //private static final String TEXTURE_PATH = Fmltutor.MODID + ":" + "textures/gui/container/gui_demo.png";
+        //private static final ResourceLocation TEXTURE = new ResourceLocation(TEXTURE_PATH);
+        //GlStateManager.color(1.0F, 1.0F, 1.0F);
+
+        //this.mc.getTextureManager().bindTexture(TEXTURE);
+        //int offsetX = (this.width - this.xSize) / 2, offsetY = (this.height - this.ySize) / 2;
+
+        //this.drawTexturedModalRect(offsetX, offsetY, 0, 0, this.xSize, this.ySize);
     }
 
     @Override
     protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY)
     {
-        // TODO
 
 
     }
