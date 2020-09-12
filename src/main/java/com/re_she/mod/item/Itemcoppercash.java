16786b895743a53f28adf695c139614f2cdf4115
RE didi <1030759339@qq.com>
2020-09-12 17:07:16 +0800

    修复gui完成

--- src/main/java/com/re_she/mod/item/Itemcoppercash.java
+++ src/main/java/com/re_she/mod/item/Itemcoppercash.java
@@ -3,9 +3,12 @@ package com.re_she.mod.item;
 import com.re_she.mod.Fmltutor;
 import com.re_she.mod.creativetab.CreativeTabsLoader;
 import com.re_she.mod.inventory.GuiElementLoader;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.effect.EntityLightningBolt;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.IChatComponent;
 import net.minecraft.world.World;
@@ -17,6 +20,8 @@ public class Itemcoppercash extends Item {
         this.setTextureName(Fmltutor.MODID + ":" + "copper_cash");//设置材质
         this.setCreativeTab(CreativeTabsLoader.re_tabFMLTutor);
     }
+
+
     @Override
     public ItemStack onItemRightClick(ItemStack itemStackIn, World worldIn, EntityPlayer playerIn)
     {
